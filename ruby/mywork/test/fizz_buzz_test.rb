8e8a2c08b1adbd36ef5ffc4ae50fa12dcdc04f88
EC2 Default User <ec2-user@ip-172-31-5-72.ap-northeast-1.compute.internal>
2023-01-26 07:37:40 +0000

    [Mywork]

--- ruby/mywork/test/fizz_buzz_test.rb
+++ ruby/mywork/test/fizz_buzz_test.rb
@@ -1,19 +1,5 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    'Fizz_Buzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-  
-  n.to_s
-  
-  end
-  
-end
-
 require 'minitest/autorun'
+  require_relative '../ib/fizz_buzz'
 
 class FizzBuzzTest<Minitest::Test
   def test_fizz_buzz
